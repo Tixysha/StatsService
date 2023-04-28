7bd74815cdb0ac2de6e1e106fb54abc074c61702
Mariya <mari.tishina@internet.ru>
2023-04-28 22:39:37 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,60 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        if (sales.length == 0) {
+            return 0;
+        }
+        return sum(sales) / sales.length;
+    }
+
+    public int monthMaxSale(long[] sales) {
+        int month = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[month]) {
+                month = i;
+            }
+
+        }
+        return month + 1;
+    }
+
+    public int monthMinSale(long[] sales) {
+        int month = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[month]) {
+                month = i;
+            }
+
+        }
+        return month + 1;
+    }
+
+    public int monhtsAnderAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public int monhtsOverAverage(long[] sales) {
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+}
